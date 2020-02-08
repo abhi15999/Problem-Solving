209552bb9e2bcd8877ee990d86579c322a65763e
Abhishek Gupta <abhishekgupta1599@gmail.com>
2020-02-08 12:52:50 +0530

    First

---
+++ Codeforces/766A.cpp
@@ -0,0 +1,18 @@
+//Abhishek Gupta | TCET,Mumbai | BE.IT
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    if (a == b)
+        cout << "-1";
+
+    else
+        cout << max(a.size(), b.size());
+    return 0;
+}
